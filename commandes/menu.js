1045a4ca39ba54b4ca5cccfd0f894a5f94e446f2
ANZAD <anzadibayana@gmail.com>
2025-06-19 07:06:01 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,1 +1,90 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
 
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../framework/zokou");
+    let coms = {};
+    let mode = "public";
+
+    if ((s.MODE).toLowerCase() !== "yes") {
+        mode = "private";
+    }
+
+    cm.map((com) => {
+        if (!coms[com.categorie]) {
+            coms[com.categorie] = [];
+        }
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+
+    let infoMsg = `
+╭━═「 *${s.BOT}* 」═━❂
+┃🔋╭────••••────➻
+┃🔋│🌏 OWNER : ${s.OWNER_NAME}
+┃🔋│🤖 PREFIX : [ ${s.PREFIXE} ]
+┃🔋│🔘 𝙼ODE : *${mode}*
+┃🔋│🔅 RAM  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
+┃🔋│◆ DATE  : *${date}*
+┃🔋│🌐 PLATFORM : ${os.platform()}
+┃🔋│☠️ DEVELOPER : KINGDOM MD
+┃🔋│⏳ COMMANDS : ${cm.length}
+┃🔋│🥇 THEME : KINGDOM 
+┃⊛└────••••────➻
+╰─━━━━══──══━━━❂\n${readmore}
+`;
+
+    let menuMsg = `KINGDON MD `;
+    
+    for (const cat in coms) {
+        menuMsg += `
+❁━━〔 *${cat}* 〕━━❁
+╭━━══••══━━••⊷
+║◆┊ `;
+        for (const cmd of coms[cat]) {
+            menuMsg += `          
+║◆┊ ${s.PREFIXE}  *${cmd}*`;    
+        }
+        menuMsg += `
+║◆┊
+╰─━━═••═━━••⊷`;
+    }
+    
+    menuMsg += `
+> ©powered by KINGDOM MD\n`;
+
+    try {
+        const senderName = nomAuteurMessage || message.from;  // Use correct variable for sender name
+        await zk.sendMessage(dest, {
+            text: infoMsg + menuMsg,
+            contextInfo: {
+                mentionedJid: [senderName],
+                externalAdReply: {
+                    title: "KINGDOM MD MENU LIST",
+                    body: "Dont worry bro I have more tap to follow",
+                    thumbnailUrl: "https://files.catbox.moe/0kwukb.jpg",
+                    sourceUrl: "https://whatsapp.com/channel/0029Vb6CC2dB4hdPp1CrYv0f",
+                    mediaType: 1,
+                    renderLargerThumbnail: true
+                }
+            }
+        });
+    } catch (error) {
+        console.error("Menu error: ", error);
+        repondre("🥵🥵 Menu error: " + error);
+    }
+});
+
+
+       
